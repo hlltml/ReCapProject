9d3d34e47378b6cc88c706b0cb873215d181b496
halil temel <haliltml91@gmail.com>
2021-02-27 16:24:23 +0300

    FluentValidation desteği eklendi

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,9 +1,12 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossingCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -23,19 +26,10 @@ namespace Business.Concrete
 
         public IResult Add(Car car)
         {
-            if (car.Description.Length >= 2 && car.DailyPrice > 0)
-            {
-                _carDal.Add(car);
-                return new SuccessResult(Messages.CarAdded);
-            }
-            else if (car.Description.Length < 2)
-            {
-                return new ErrorResult(Messages.CarNameInvalid);
-            }
-            else
-            {
-                return new ErrorResult(Messages.CarDailyPriceInvalid);
-            }
+            ValidationTool.Validate(new CarValidator(), car);
+
+            _carDal.Add(car);
+            return new SuccessResult(Messages.CarAdded);
         }
 
         public IResult Update(Car car)
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator:AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty(); //NotEmpty: Required and must have a non-zero length || NotNull:Required but can have a zero length
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Email).EmailAddress();
+            RuleFor(u => u.Password).Length(5, 15);
+        }
+    }
+}
